64ce4ab95f7825fe4fd5f744ad673ca5d29be801
Alon Dahari <climbinghobo@gmail.com>
2015-02-23 12:11:08 -0700

    add mongoose

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose')
 
 var app = express();
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser());
+mongoose.connect('mongodb://localhost/omega3')
+
+var Applicant = mongoose.model('Applicant', {
+	name: String,
+	bio: String,
+	skills: String,
+	experience: Number,
+	reason: String
+})
 
 app.get('/', function(req, res) {
 	res.render('index');
@@ -22,8 +32,10 @@ app.get('/form-submitted', function(req, res){
 
 // creates and applicant
 app.post('/applicant', function(req, res){
-	console.log(req.body);
+	var applicant = new Applicant(req.body)
+	applicant.save()
 	res.redirect('/form-submitted');
+
 });
 
 var server = app.listen(8441, function() {
